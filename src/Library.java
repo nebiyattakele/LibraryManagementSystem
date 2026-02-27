d4ad6575b0c3f07d464abaf8eef958f2658aa31f
nebiyattakele <nebiyattakele23@gmail.com>
2026-02-27 09:59:14 +0300

    Add Library class with addBook functionality

--- src/Library.java
+++ src/Library.java
@@ -1,3 +1,15 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Library {
-    
+    private List<Book> books;
+
+    public Library() {
+        books = new ArrayList<>();
+    }
+
+    public void addBook(Book book) {
+        books.add(book);
+        System.out.println("Added: " + book.getTitle());
+    }
 }
