b22a2d31d31d93ffa36c401a4e940daf3b41645e
nebiyattakele <nebiyattakele23@gmail.com>
2026-02-27 09:55:43 +0300

    Add Book class with fields and constructor

--- src/Book.java
+++ src/Book.java
@@ -1,3 +1,15 @@
 public class Book {
-    
+    private String title;
+    private String author;
+    private boolean isBorrowed;
+
+    public Book(String title, String author) {
+        this.title = title;
+        this.author = author;
+        this.isBorrowed = false;
+    }
+
+    public String getTitle() { return title; }
+    public String getAuthor() { return author; }
+    public boolean isBorrowed() { return isBorrowed; }
 }
